7a2449c60245570a0aba954e87dae313cac9137c
정재영 <jjy600901@snu.ac.kr>
2021-08-01 11:06:19 +0900

    cmd scheduler

--- agent.py
+++ agent.py
@@ -13,11 +13,14 @@ from modules.processorFactory import ProcessorFactory
 import bson
 from multiprocessing import Process
 
+from collections import defaultdict, deque
+
 SERVER_IP = "0.0.0.0"
 SERVER_PORT = 9000
 
 class Agent(object):
     BUF_SIZE = 0x1000
+    per_port_queue = defaultdict(deque)  # 동일한 포트면 동작하지 않도록
 
     def __init__(self):
         self.ip = utility.get_local_ip()
@@ -30,6 +33,42 @@ class Agent(object):
     def connect_to_server(self):
         self.sock = utility.remote(SERVER_IP, SERVER_PORT)
 
+
+    def _scheduler(self, cmd):
+        Q = self.per_port_queue
+
+        '''
+        {
+            "type": "unlock",
+            "port": 445
+        }
+        '''
+        if cmd['type'] == 'unlock':
+            port = cmd['port']
+            Q[port].popleft()
+            if Q[port]: # 기다리는 명령어가 있다면
+                return Q[port][0]
+
+            return None
+
+        if cmd['type'] not in ['defense', 'attack_secu']:
+            return cmd
+
+        port = 0
+        if cmd['type'] == 'defense':
+            port = cmd['port']
+        elif cmd['type'] == 'attack_secu':
+            port = cmd['dst_port']
+
+        if port == 0:   # use ephermeral port
+            return cmd
+
+        # 이미 사용중인 port 라면 None 을 리턴
+        result_cmd = None if Q[port] else cmd
+        Q[port].append(cmd)
+        return result_cmd
+
+
     # _run 은 단일 프로세스
     def _run(self):
         logger.info("[agent] wait cmd...")
@@ -41,10 +80,13 @@ class Agent(object):
 
         logger.debug(f"bson: {cmd}")
         cmd = bson.loads(cmd)
-        logger.debug(f"dict: {cmd}")
+        logger.info(f"cmd: {cmd}")
 
-        p = Process(target = self._process_cmd, args = (cmd, ))
-        p.start()
+        s_cmd = self._scheduler(cmd)
+
+        if s_cmd:
+            p = Process(target = self._process_cmd, args = (s_cmd, ))
+            p.start()
 
 
     def _process_cmd(self, cmd):
