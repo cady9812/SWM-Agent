86744e98cc9b7f61b972a1ff84929d83e0889ed5
정재영 <jjy600901@snu.ac.kr>
2021-08-10 00:28:33 +0900

    [kvm controller] skeleton

--- modules/kvm_manager.py
+++ modules/kvm_manager.py
@@ -37,18 +37,53 @@ class KvmManager(Processor):
 
     def list_snapshots(self, domain):
         result = self.localhost.list_snapshots(domain)
-        print(result)
-        return
+        logger.info(f"[snapshot - list {domain}] {result}")
+        return result
+
+
+    def create_snapshot(self, domain, new_name):
+        result = self.localhost.snapshot.create_as(domain, new_name)
+        logger.info(f"[snapshot - create {domain} {new_name}] {result}")
+        return result
+
+
+    def delete_snapshot(self, domain, snapshot_name):
+        result = self.localhost.snapshot.delete(domain, snapshot_name)
+        logger.info(f"[snapshot - delete {domain} {snapshot_name}] {result}")
+        return result
 
 
     def revert_snapshot(self, domain, snapshot_name):
         result = self.localhost.snapshot.revert(domain, snapshot_name)
+        logger.info(f"[snapshot - revert {domain} {snapshot_name}] {result}")
         return result
 
+
+    def start_vm(self, domain):
+        result = self.localhost.domain.start(domain)
+        logger.info(f"[vm - start {domain}] {result}")
+        return result
+
+    
+    def shutdown_vm(self, domain):
+        # shutdown 을 쓰면 강제로는 안꺼져서 destroy 사용
+        result = self.localhost.domain.destroy(domain)
+        logger.info(f"[vm - shutdown {domain}] {result}")
+        return result
+
+
+    def state_vm(self, domain):
+        result = self.localhost.domain.state(domain)
+        logger.info(f"[vm - state {domain}] {result}")
+        return result
 
 if __name__ == '__main__':
     msg = {
         "type" : "kvm", # 보안장비
     }
-    kmanager = KvmManager(msg)
-    kmanager.list_snapshots('win10')
+    k = KvmManager(msg)
+    k.start_vm('win10')
+    from time import sleep
+    sleep(20)
+    k.shutdown_vm('win10')
+    k.state_vm('win10')
